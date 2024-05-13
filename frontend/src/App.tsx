949abc2c348cfab9ff29d89ac0ac49cf006c91af
Omar Brikaa <brikaaomar@gmail.com>
2024-05-13 17:56:33 +0300

    WIP frontend, login

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
-function App() {
+interface RequestOptions {
+  method: string;
+  headers: { [key: string]: string };
+  body?: any;
+}
+
+interface RequestContext {
+  id: string;
+  name: string;
+  email: string;
+  role: 'ADMIN' | 'INSTRUCTOR' | 'STUDENT';
+}
+
+const sendRequest = async (authToken: string | null, method: string, url: string, body?: any) => {
+  const opts: RequestOptions = {
+    method,
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+  if (method.toLowerCase() !== 'get' && method.toLowerCase() !== 'delete')
+    opts.body = JSON.stringify(body);
+  if (authToken !== null) opts.headers['Authorization'] = `Basic ${authToken}`;
+  const res = await fetch(url, opts);
+  if (res.status >= 400) alert(`An error has occurred: ${await res.text()}`);
+  return res;
+};
+
+const LogoutButton = (props: { setNavbar: (navbar: JSX.Element) => void }) => {
+  const logout = () => {
+    props.setNavbar(<GuestNavbar setNavbar={props.setNavbar} />);
+  };
+  return <button onClick={logout}>Log out</button>;
+};
+
+const AdminNavbar = (props: { setNavbar: (navbar: JSX.Element) => void }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Hello, admin - <LogoutButton setNavbar={props.setNavbar} />
     </div>
   );
+};
+
+const GuestNavbar = (props: { setNavbar: (navbar: JSX.Element) => void }) => {
+  const login = async () => {
+    const nameOrEmail = prompt('Name or email');
+    if (nameOrEmail === null) return;
+    const password = prompt('Password');
+    if (password === null) return;
+    const res = await sendRequest(null, 'POST', '/api/user/login', { nameOrEmail, password });
+    if (res.status !== 200) return;
+
+    const token = (await res.json())['token'] as string;
+    const ctxRes = await sendRequest(token, 'GET', '/api/user/user');
+    if (ctxRes.status !== 200) return;
+
+    const ctx = (await ctxRes.json()) as RequestContext;
+    if (ctx.role === 'ADMIN') {
+      props.setNavbar(<AdminNavbar setNavbar={props.setNavbar} />);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={login}>Login</button> - <button>Register</button>
+    </div>
+  );
+};
+
+function App() {
+  const [navbar, setNavbar] = useState<JSX.Element | null>(null);
+  useEffect(() => {
+    setNavbar(<GuestNavbar setNavbar={setNavbar} />);
+  }, []);
+  return navbar;
 }
 
 export default App;
